4b8891f396151b9e7502deb4acf797c5ea1655ee
Renuka Sanjeev Bhure <72491412+renuka142@users.noreply.github.com>
2022-05-12 20:45:58 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 import cv2
 
-data = pd.read_csv("/home/saireddy/Desktop/capstone/fer2013/fer2013.csv", delimiter=",")
+data = pd.read_csv("/home/user/Desktop/capstone/fer2013/fer2013.csv", delimiter=",")
 
 
 emotion = data['emotion']
@@ -26,14 +26,14 @@ for i in range(len(Usage_path)):
 label_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 for i in range(len(label_names)):
-	if not os.path.exists(os.path.join("/home/saireddy/Desktop/capstone/fer2013/Training/", label_names[i])):
-		os.mkdir(os.path.join("/home/saireddy/Desktop/capstone/fer2013/Training/", label_names[i]))
+	if not os.path.exists(os.path.join("/home/user/Desktop/capstone/fer2013/Training/", label_names[i])):
+		os.mkdir(os.path.join("/home/user/Desktop/capstone/fer2013/Training/", label_names[i]))
 		
-	if not os.path.exists(os.path.join("/home/saireddy/Desktop/capstone/fer2013/PublicTest/", label_names[i])):
-		os.mkdir(os.path.join("/home/saireddy/Desktop/capstone/fer2013/PublicTest/", label_names[i]))
+	if not os.path.exists(os.path.join("/home/user/Desktop/capstone/fer2013/PublicTest/", label_names[i])):
+		os.mkdir(os.path.join("/home/user/Desktop/capstone/fer2013/PublicTest/", label_names[i]))
 		
-	if not os.path.exists(os.path.join("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/", label_names[i])):
-		os.mkdir(os.path.join("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/", label_names[i]))
+	if not os.path.exists(os.path.join("/home/user/Desktop/capstone/fer2013/PrivateTest/", label_names[i])):
+		os.mkdir(os.path.join("/home/user/Desktop/capstone/fer2013/PrivateTest/", label_names[i]))
 		
 		
 label = emotion.values
@@ -47,37 +47,37 @@ for i in range(len(label)):
 	if label[i] == 0 and Usage_[i] == 'Training':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/Training/Angry/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/Training/Angry/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 1 and Usage_[i] == 'Training':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/Training/Disgust/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/Training/Disgust/00"+str(i)+'.jpg', img)
 	
 	if label[i] == 2 and Usage_[i] == 'Training':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/Training/Fear/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/Training/Fear/00"+str(i)+'.jpg', img)
 	
 	if label[i] == 3 and Usage_[i] == 'Training':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/Training/Happy/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/Training/Happy/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 4 and Usage_[i] == 'Training':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/Training/Sad/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/Training/Sad/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 5 and Usage_[i] == 'Training':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/Training/Suprise/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/Training/Suprise/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 6 and Usage_[i] == 'Training':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/Training/Neutral/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/Training/Neutral/00"+str(i)+'.jpg', img)
 		
 		
 
@@ -86,70 +86,70 @@ for i in range(len(label)):
 	if label[i] == 0 and Usage_[i] == 'PublicTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PublicTest/Angry/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PublicTest/Angry/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 1 and Usage_[i] == 'PublicTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PublicTest/Disgust/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PublicTest/Disgust/00"+str(i)+'.jpg', img)
 	
 	if label[i] == 2 and Usage_[i] == 'PublicTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PublicTest/Fear/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PublicTest/Fear/00"+str(i)+'.jpg', img)
 	
 	if label[i] == 3 and Usage_[i] == 'PublicTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PublicTest/Happy/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/usery/Desktop/capstone/fer2013/PublicTest/Happy/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 4 and Usage_[i] == 'PublicTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PublicTest/Sad/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PublicTest/Sad/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 5 and Usage_[i] == 'PublicTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PublicTest/Suprise/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PublicTest/Suprise/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 6 and Usage_[i] == 'PublicTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PublicTest/Neutral/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PublicTest/Neutral/00"+str(i)+'.jpg', img)
 		
 for i in range(len(label)):
 	if label[i] == 0 and Usage_[i] == 'PrivateTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/Angry/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PrivateTest/Angry/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 1 and Usage_[i] == 'PrivateTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/Disgust/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PrivateTest/Disgust/00"+str(i)+'.jpg', img)
 	
 	if label[i] == 2 and Usage_[i] == 'PrivateTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/Fear/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PrivateTest/Fear/00"+str(i)+'.jpg', img)
 	
 	if label[i] == 3 and Usage_[i] == 'PrivateTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/Happy/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PrivateTest/Happy/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 4 and Usage_[i] == 'PrivateTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/Sad/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PrivateTest/Sad/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 5 and Usage_[i] == 'PrivateTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/Suprise/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PrivateTest/Suprise/00"+str(i)+'.jpg', img)
 		
 	if label[i] == 6 and Usage_[i] == 'PrivateTest':
 		print(i)
 		img = images[i].reshape(48, 48)
-		cv2.imwrite("/home/saireddy/Desktop/capstone/fer2013/PrivateTest/Neutral/00"+str(i)+'.jpg', img)
+		cv2.imwrite("/home/user/Desktop/capstone/fer2013/PrivateTest/Neutral/00"+str(i)+'.jpg', img)
